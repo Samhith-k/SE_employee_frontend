8e002b9ef0477b6e2745a3ac7b20b49a735166b3
Samhith-k <39161002+Samhith-k@users.noreply.github.com>
2019-11-17 18:06:16 +0530

    Add files via upload
    logout

--- src/MyNavbar.js
+++ src/MyNavbar.js
@@ -15,6 +15,7 @@ const MyNavbar = () => {
           <Nav.Link><Link to="/Bill" style={{color:'white'}}>Bill-Rimburse</Link></Nav.Link>
           <Nav.Link><Link to="/Cab" style={{color:'white'}}>Book-Cab</Link></Nav.Link>
           <Nav.Link><Link to="/Approve" style={{color:'white'}}>Approvals</Link></Nav.Link>
+		  <Nav.Link><Link to="/l"  onclick={ localStorage.clear() }  style={{color:'white'}}>Logout</Link></Nav.Link>
           
 
         </Nav>
